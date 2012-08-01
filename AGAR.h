ed21b0ea3b2f6fcc58b931487f2e104406a45bc5
Runik <34705093+rtoumazet@users.noreply.github.com>
2012-08-01 12:01:33 +0200

    First update

---
+++ AGAR.h
@@ -0,0 +1,33 @@
+#ifndef _AGAR_AGAR_h
+#define _AGAR_AGAR_h
+
+#include <CtrlLib/CtrlLib.h>
+#include <plugin/sqlite3/Sqlite3.h>
+#include <SqlCtrl/SqlCtrl.h>
+
+
+using namespace Upp;
+
+#define SCHEMADIALECT <plugin/sqlite3/Sqlite3Schema.h>
+#define MODEL <AGAR/AGAR.sch>
+#include "Sql/sch_header.h"
+
+#define LAYOUTFILE <AGAR/AGAR.lay>
+#include <CtrlCore/lay.h>
+
+class AGAR : public WithAGARLayout<TopWindow> {
+	//WithModifyLayout<ParentCtrl> modify;
+
+public:
+	// Main menu
+	MenuBar menu;
+	void MainMenu( Bar& bar);
+	void SubMenu(Bar& bar);
+	void MakerList();
+	void Exit();
+	
+	typedef AGAR CLASSNAME;
+	AGAR();
+};
+
+#endif
