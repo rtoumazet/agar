f53917c69f9b3f82216d541a0e88fbbeca9e3678
Runik <34705093+rtoumazet@users.noreply.github.com>
2013-04-30 17:23:23 +0200

    - WIP to display colors in the pcb table

--- dialogs/pcbs.cpp
+++ dialogs/pcbs.cpp
@@ -19,9 +19,41 @@ PcbsDlg::PcbsDlg() {
 	TAB_pcbs.AddColumn(LOCATION_ID,t_("Location"));
 	TAB_pcbs.WhenLeftDouble = THISBACK(Edit);
 
-	//TAB_pcbs.SetOrderBy(LABEL);
-	
 	TAB_pcbs.Query();
+	
+	//TAB_pcbs.HeaderObject().HideTab(2); // state is hidden
+	// formatting text
+	String str;
+	for (int i=0; i<TAB_pcbs.GetCount(); i++) {
+		str = TAB_pcbs.Get(i,1).ToString();
+		
+		
+	}
+	
+/*	Sql sql;
+	sql.Execute("select ID,GAME_ID,PCB_TYPE_ID,PCB_STATE_ID,TAG,LOCATION_ID from PCB);
+	while (sql.Fetch() {
+		
+		
+		
+		TAB_pcbs.Add(
+			sql[0], // record id.
+			
+	}
+	
+	sql.Execute("select ID,LABEL,INK,PAPER from PCB_STATE");
+	while(sql.Fetch()) {
+		ink = Color::FromRaw(static_cast<dword>(sql[2].To<int64>()));
+		paper = Color::FromRaw(static_cast<dword>(sql[3].To<int64>()));
+		
+		TAB_pcbStateArray.Add(
+			sql[0], // record id
+			AttrText(sql[1].ToString()).Ink(ink).Paper(paper), // formatted text
+			sql[1].ToString(), // raw text
+			paper, // background color
+			ink // text color
+		);
+	}*/
 }
 
 void PcbsDlg::OwnMenu(Bar& bar) {
