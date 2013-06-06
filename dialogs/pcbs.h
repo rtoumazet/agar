3edd756449d008da25827775996565294622281d
Runik <34705093+rtoumazet@users.noreply.github.com>
2013-06-06 14:07:21 +0200

    Added filters on the pcb table

--- dialogs/pcbs.h
+++ dialogs/pcbs.h
@@ -9,12 +9,20 @@ class PcbsDlg : public WithPcbsLayout<TopWindow> {
 		void Edit(int pcbId=0);
 		void Remove();
 
+		void OwnMenu(Bar& bar);
+
+		// Sort functions and variables
+		bool isSortedAsc_;
 		void ReloadTable(const bool& ascSort);
 		void SortTable(const int& i);
 		
-		void OwnMenu(Bar& bar);
+		// Filter functions and variables
+		int 					filterState_;
+		//Array<Label> 			label_;
+		ArrayMap<int, Option>  	option_;
 		
-		bool isSortedAsc_;
+		void 					LoadFaultData();
+		void 					ExecuteFilter();
 
 	public:
 		PcbsDlg();	
