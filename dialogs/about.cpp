441afd33495a028150819a46e9e9da94d596a446
Runik <34705093+rtoumazet@users.noreply.github.com>
2013-12-23 22:40:02 +0100

    Added config file path to about dialog Added version field to pcb dialog

--- dialogs/about.cpp
+++ dialogs/about.cpp
@@ -7,5 +7,7 @@ AboutDlg::AboutDlg(const String& version) {
 	String tmp = Format("[+200 [* %1",version);
 	tmp += "]]&Created by Runik&";
 	tmp += "[^https://bitbucket.org/Runik/agar/wiki/Home^ Homepage]";
+	tmp += "&Config file path : ";
+	tmp += DeQtf(ConfigFile("AGAR.db"));
 	RTC_About = tmp;
 }
