624f590c876bf29f4607ae29b11dfb1bf3a080c9
Runik <Runik@localhost>
2014-01-10 14:36:58 +0100

    Fixed truncated scroll bar and last line in the analysis & actions table

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ SqlId count("count(*)");
 
 AGAR::AGAR()
 {
-	version_ = "AGAR 1.1.7";
+	version_ = "AGAR 1.1.8";
 	
 	SetDateFormat("%3:02d/%2:02d/%1:4d"); // set dd/mm/yyyy format
 	SetDateScan("dmy"); // same but for input
