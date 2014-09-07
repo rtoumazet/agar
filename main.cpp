99d3bbd08cc806db812b66ce2b13fbcaf5594b50
Runik <34705093+rtoumazet@users.noreply.github.com>
2014-09-07 16:43:34 +0200

    Version update

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ SqlId count("count(*)");
 
 AGAR::AGAR()
 {
-	version_ = "AGAR 1.1.9";
+	version_ = "AGAR 1.1.9a";
 	
 	SetDateFormat("%3:02d/%2:02d/%1:4d"); // set dd/mm/yyyy format
 	SetDateScan("dmy"); // same but for input
