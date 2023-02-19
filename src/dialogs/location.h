c48382f0a92566d44a7359e92b034490efe2cd1e
Runik <34705093+rtoumazet@users.noreply.github.com>
2023-02-19 11:57:54 +0100

    Update location.h

--- src/dialogs/location.h
+++ src/dialogs/location.h
@@ -2,12 +2,10 @@
 #include "../enums.h"
 
 class LocationDlg : public WithLocationLayout<TopWindow> {
-
 	typedef LocationDlg CLASSNAME;
 	
 	public:
 		SqlCtrls ctrls;
 		
 		LocationDlg(const OpeningType type);
-
 };
